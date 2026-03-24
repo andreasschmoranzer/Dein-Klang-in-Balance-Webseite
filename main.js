e00a77347b45d38723677bdaab2b6c6a03ce4ca5
andreas schmoranzer <andreas.schmoranzer@web.de>
2026-03-24 21:12:10 +0100

    Add some changes to the nav-menu

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-const navigationMenu = document.querySelector(".nav-container");
+function overlayOn() {
+  document.getElementById("nav-menu").style.display = "block";
+}
 
-function openNav() {
-  const html = `<div class="nav-content">
-        <div class="nav-items">
-          <p class="nav-item">Über mich</p>
-          <p class="nav-item">Kurse</p>
-          <p class="nav-item">Kontakt</p>
-        </div>
-        <div class="nav-close">
-          <button class="nav-close-button">X</button>
-        </div>
-      </div>
-    `;
-
-  navigationMenu.innerHTML = html;
+function overlayOff() {
+  document.getElementById("nav-menu").style.display = "none";
 }
