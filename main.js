027412f4a77ed2a5c7094c9b9c067858f1c56d2c
andreas schmoranzer <andreas.schmoranzer@web.de>
2026-04-04 16:06:47 +0200

    Add the categories section

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const navMenuOpenEl = document.querySelector("#nav-open");
+const navMenuCloseEl = document.querySelector("#nav-close");
+
+navMenuOpenEl.addEventListener("click", () => overlayOn());
+navMenuCloseEl.addEventListener("click", () => overlayOff());
+
 function overlayOn() {
   document.getElementById("nav-menu").style.display = "block";
 }
